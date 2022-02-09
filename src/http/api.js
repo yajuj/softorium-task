2ad30c14b4f87652b426d22acaf0aefe522d0bbd
yajuj <noticethatred@gmail.com>
2022-02-09 12:42:39 +0500

    minor changes

--- src/http/api.js
+++ src/http/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://testtask.softorium.pro/',
-  headers: { 'X-APP-ID': localStorage.getItem('uuid') },
+  headers: { 'X-APP-ID': localStorage.getItem('id') },
 });
 
 api.interceptors.request.use(config => {
